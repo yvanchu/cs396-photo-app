64ac00003d730e27639097dce290346551621aa5
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-02-14 15:32:55 -0600

    hw04 initial file set up

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-# load the environment variables:
 from dotenv import load_dotenv
 load_dotenv()
-
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api
+from flask_cors import CORS
+from flask import render_template
 import os
-from models import db, User
+from models import db, User, ApiNavigator
 from views import bookmarks, comments, followers, following, \
     posts, profile, stories, suggestions, post_likes
 
+
+
 app = Flask(__name__)
+
+# CORS: allows anyone from anywhere to use your API:
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False    
@@ -34,10 +39,25 @@ profile.initialize_routes(api)
 stories.initialize_routes(api)
 suggestions.initialize_routes(api)
 
+
 # Server-side template for the homepage:
 @app.route('/')
 def home():
-    return 'Your API'
+    return render_template(
+        'starter-client.html', 
+        user=app.current_user
+    )
+
+@app.route('/api')
+def api_docs():
+    navigator = ApiNavigator(app.current_user)
+    return render_template(
+        'api/api-docs.html', 
+        user=app.current_user,
+        endpoints=navigator.get_endpoints(),
+        url_root=request.url_root[0:-1] # trim trailing slash
+    )
+
 
 
 # enables flask app to run using "python3 app.py"
--- views/posts.py
+++ views/posts.py
@@ -1,11 +1,10 @@
-from distutils.log import error
 from flask import Response, request
 from flask_restful import Resource
-from models import Post, User, db
-from . import can_view_post, get_authorized_user_ids
+from models import Post, db
+
+from views import security, get_authorized_user_ids
+
 import json
-from sqlalchemy import and_
-from my_decorators import id_is_integer_or_400_error
 
 def get_path():
     return request.host_url + 'api/posts/'
@@ -16,44 +15,32 @@ class PostListEndpoint(Resource):
         self.current_user = current_user
 
     def get(self):
-        # TODO: 
-        # DONE 1. No security implemented; 
-        # DONE 2. limit is hard coded (versus coming from the query parameter)
-        # DONE 3. No error checking using wrapper
-        authorized_user_ids = get_authorized_user_ids(self.current_user)
-
-        # might need to use wrappers
-        if "limit" in request.args:
+        ids = get_authorized_user_ids(self.current_user)
+        posts = Post.query.filter(Post.user_id.in_(ids))
+        limit = request.args.get('limit')
+        if limit:
             try:
-                limitNumber = int(request.args["limit"])
-                if limitNumber > 50:
-                    raise NameError("limit greater than 50")
+                limit = int(limit)
             except:
-                return Response(
-                json.dumps({'message': '{0} must be an integer less than 50.'.format(request.args["limit"])}), 
-                mimetype="application/json", 
-                status=400
-                )
+                return Response(json.dumps({'message': 'Limit must be an integer between 1 and 50'}), mimetype="application/json", status=400)
+            if limit > 50 or limit < 1:
+                return Response(json.dumps({'message': 'Limit must be an integer between 1 and 50'}), mimetype="application/json", status=400)
         else:
-            limitNumber = 10
-        data = Post.query.filter(Post.user_id.in_(authorized_user_ids)).limit(limitNumber).all()
+            limit = 10
+        posts = posts.order_by(Post.pub_date.desc()).limit(limit)
 
+        # note: if you pass the current user into to_dict(), it will tell you
+        # whether or not the current user liked and/or bookmarked any of the posts
         data = [
-            item.to_dict() for item in data
+            item.to_dict(user=self.current_user) for item in posts.all()
         ]
         return Response(json.dumps(data), mimetype="application/json", status=200)
-
-
 
     def post(self):
         body = request.get_json()
         image_url = body.get('image_url')
-        if not image_url:
-            return Response(
-                json.dumps({'message': 'image_url is required'}), 
-                mimetype="application/json", 
-                status=400
-                )
+        if image_url is None:
+            return Response(json.dumps({'message': '"image_url" is required.'}), mimetype="application/json", status=400)
         caption = body.get('caption')
         alt_text = body.get('alt_text')
         user_id = self.current_user.id # id of the user who is logged in
@@ -62,56 +49,46 @@ class PostListEndpoint(Resource):
         post = Post(image_url, user_id, caption, alt_text)
         db.session.add(post)
         db.session.commit()
-        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=201)
+        return Response(json.dumps(post.to_dict(user=self.current_user)), mimetype="application/json", status=201)
         
 class PostDetailEndpoint(Resource):
 
     def __init__(self, current_user):
         self.current_user = current_user
         
-    @id_is_integer_or_400_error
+    
+    @security.id_is_valid
+    @security.user_can_edit_post
     def patch(self, id):
         post = Post.query.get(id)
-
-        # a user can only edit their own post:
-        if not post or post.user_id != self.current_user.id:
-            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
-       
-
         body = request.get_json()
-        post.image_url = body.get('image_url') or post.image_url
-        post.caption = body.get('caption') or post.caption
-        post.alt_text = body.get('alt_text') or post.alt_text
+
+        post.image_url = body.get('image_url') or body.get('image_url')
+        if body.get('caption') is not None:
+            post.caption = body.get('caption')
+        if body.get('alt_text') is not None:
+            post.alt_text = body.get('alt_text')
         
         # commit changes:
         db.session.commit()        
-        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
+        return Response(json.dumps(post.to_dict(user=self.current_user)), mimetype="application/json", status=200)
     
-    @id_is_integer_or_400_error
+    @security.id_is_valid
+    @security.user_can_edit_post
     def delete(self, id):
-
-        # a user can only delete their own post:
-        post = Post.query.get(id)
-        if not post or post.user_id != self.current_user.id:
-            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
-       
-
         Post.query.filter_by(id=id).delete()
         db.session.commit()
+
         serialized_data = {
             'message': 'Post {0} successfully deleted.'.format(id)
         }
         return Response(json.dumps(serialized_data), mimetype="application/json", status=200)
 
-    @id_is_integer_or_400_error
+    @security.id_is_valid
+    @security.user_can_view_post
     def get(self, id):
         post = Post.query.get(id)
-
-        # if the user is not allowed to see the post or if the post does not exist, return 404:
-        if not post or not can_view_post(post.id, self.current_user):
-            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
-        
-        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
+        return Response(json.dumps(post.to_dict(user=self.current_user)), mimetype="application/json", status=200)
 
 def initialize_routes(api):
     api.add_resource(
@@ -124,4 +101,3 @@ def initialize_routes(api):
         '/api/posts/<id>', '/api/posts/<id>/',
         resource_class_kwargs={'current_user': api.app.current_user}
     )
-
